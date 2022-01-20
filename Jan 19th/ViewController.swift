c5cde05e23407965733193bca6b2d078334fca8c
Nathaniel Whittington <whittinn@gmail.com>
2022-01-19 21:32:58 -0600

    commit

--- Jan 19th/ViewController.swift
+++ Jan 19th/ViewController.swift
@@ -14,6 +14,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func splitArray(_ group: [Int])-> [(Int,Int)]{
+        
+        let output = stride(from: 0, to: group.count - 1 , by: 2).map({(group[$0],group[$0+1])})
+        
+        return output
+        
+    }
+    
+
+    func factorial(_ number:Int)-> Int{
+       
+        var startingNumber = 1
+        var holder = 0
+        
+        for num in 1...number{
+            
+            startingNumber = 1 * num
+            holder += startingNumber
+            
+        }
+        return holder
+
+    }
+
+
+    func countChar(_ letters:[String], _ char:String) -> Int{
+        
+       var count = 0
+        
+        for letter in letters{
+            
+            if letter == char{
+                count += 1
+            }
+        }
+        return count
+    }
+
+
+    
 
 }
 
